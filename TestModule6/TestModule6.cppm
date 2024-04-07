54e9dfe8237c461deedc6ba88f99cbae7df7d515
Shyfan <410713847@qq.com>
2024-04-07 17:56:13 +0800

    保存

---
+++ TestModule6/TestModule6.cppm
@@ -0,0 +1,21 @@
+﻿// TestModule1.cppm
+
+module;
+
+#include <iostream>
+
+export module TestModule6;
+
+export class TestModule6Class
+{
+public:
+    void Print1()
+    {
+        std::cout << "TestModule6Class::Print1 \n";
+    }
+
+    void Print2()
+    {
+        std::cout << "TestModule6Class::Print2 \n";
+    }
+};
--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ import TestModule2.Print2;
 import TestModule3;
 import TestModule4;
 import TestModule5;
+import TestModule6;
 
 int main()
 {
@@ -24,6 +25,10 @@ int main()
     TestModule5_Print2();
     TestModule5_Print3_1();
     TestModule5_Print3_2();
+
+    TestModule6Class testModule6Class;
+    testModule6Class.Print1();
+    testModule6Class.Print2();
 
     return 0;
 }
